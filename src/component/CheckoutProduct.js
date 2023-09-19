1cd8153f7145da2a11c907ff92fdffb90dcad3f1
channy <ox043440@gmail.com>
2023-09-19 18:23:06 +0900

    230919

---
+++ src/component/CheckoutProduct.js
@@ -0,0 +1,35 @@
+import React from "react";
+import '../style/checkoutproduct.css'
+
+function CheckoutProduct({id, image, title, price, rating}) {
+    return(
+        <div className="checkoutProduct">
+            <img className="checkoutProduct_img" src="{image}" alt="" />
+
+            <div className="checkoutProduct_info">
+                <p className="checkoutProduct_title">
+                    {title}
+                </p>
+                <p className="checkoutProduct_price">
+                    <small>w</small>
+                    <strong> {price} </strong>
+                    <small>원</small>
+                </p>
+
+                <div className="checkoutProduct_rating">
+                    {
+                            Array(rating)
+                            .fill()
+                            .map(() => (
+                                <p>*</p>
+                            ))
+                        }
+                </div>
+
+                <button>장바구니에서 제거하기</button>
+            </div>
+        </div>
+    );
+}
+
+export default CheckoutProduct;
--- src/page/Cart.js
+++ src/page/Cart.js
@@ -1,9 +1,13 @@
 import React from "react";
+import CheckoutProduct from "../component/CheckoutProduct";
+import { useStateValue } from "../component/StateProvider";
 import Subtotal from "../component/Subtotal";
 import '../style/cart.css'
 
 
 function Cart() {
+    const [{basket}, dispatch] = useStateValue();
+
     return(
         <div className="cart">
             <div className="cart_left">
@@ -11,8 +15,9 @@ function Cart() {
 
                 <div>
                     <h2 className="cart_title">장바구니입니다</h2>
-
-                    {/* 장바구니아이템 */}
+                    {basket.map(item => (
+                        <CheckoutProduct id={item.id} title={item.title} image={item.image} price={item.price} rating={item.rating} />
+                    ))}
                 </div>
             </div>
 
